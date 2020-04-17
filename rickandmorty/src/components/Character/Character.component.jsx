1d65d532d119bf805ba36884389e5af44f97e7b4
Blayze <blayzestone@protonmail.com>
2020-04-17 10:21:59 -0700

    Feat: create Character component
    The Character component creates a card with reactstrap and displays the
    data for that character passed as a props.

--- rickandmorty/src/components/Character/Character.component.jsx
+++ rickandmorty/src/components/Character/Character.component.jsx
@@ -1,9 +1,23 @@
 // Write your Character component here
 import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 const Character = ({ characterData }) => {
   return(
-    <h1>{characterData.name}</h1>
+    <div style={{width: "30%"}}>
+      <Card>
+        <CardImg top width="100%" src={characterData.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{characterData.name}</CardTitle>
+          <CardSubtitle>Species: {characterData.species}</CardSubtitle>
+          <CardSubtitle>Status: {characterData.status}</CardSubtitle>
+          <CardSubtitle>Location: {characterData.location.name}</CardSubtitle>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
 
